66e3838759e8d184d708d00c7505cc93b0847442
ChenYong <chenyong@jimeng.io>
2019-05-22 19:39:07 +0800

    export space and button; alpha release

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { default as GroupedTimeline, ITimelineGroup } from "./grouped-timeline";
 export { default as ImageViewer } from "./image-viewer";
+export { default as Space } from "./space";
+export { default as JimoButton } from "./jimo-button";
